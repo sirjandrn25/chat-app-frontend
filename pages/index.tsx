3c0ebed3cd8a84fd557ca8b9f22b3e6d5f807234
sirjandrn25 <56628702+sirjandrn25@users.noreply.github.com>
2023-04-09 10:23:23 +0545

    fixed auth redirect issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,46 @@ import React from "react";
 import { useTheme } from "../src/Context/useTheme.context";
 import Button from "../src/Components/Button/button.component";
 import { themes } from "../src/Constants/theme.constant";
+import { useEffectOnce } from "react-use";
+import useNavigation from "../src/Hooks/useNavigation.hook";
+import { AUTH_ROUTE } from "../src/Constants/route.constant";
 
 const Home = () => {
-	const { theme, toggleTheme, onChangeTheme } = useTheme();
-	const ThemeBox = ({ theme }: any) => {
-		return (
-			<div
-				className="p-6 cursor-pointer rounded-lg border "
-				onClick={() => onChangeTheme(theme)}
-				data-theme={theme}
-			>
-				<span className="capitalize">{theme}</span>
-			</div>
-		);
-	};
+  const { theme, toggleTheme, onChangeTheme } = useTheme();
+  const { navigation } = useNavigation();
 
-	return (
-		<div className="h-screen w-screen">
-			<Button onClick={toggleTheme}>
-				<div className="capitalize">
-					{" "}
-					Switch To {theme === "dark" ? "Light" : "Dark"}{" "}
-				</div>
-			</Button>
-			<div className="flex items-center gap-4">
-				{themes.map((theme: string) => {
-					return <ThemeBox key={theme} theme={theme} />;
-				})}
-			</div>
-		</div>
-	);
+  useEffectOnce(() => {
+    navigation({
+      pathname: AUTH_ROUTE,
+    });
+  });
+  const ThemeBox = ({ theme }: any) => {
+    return (
+      <div
+        className="p-6 border rounded-lg cursor-pointer "
+        onClick={() => onChangeTheme(theme)}
+        data-theme={theme}
+      >
+        <span className="capitalize">{theme}</span>
+      </div>
+    );
+  };
+
+  return (
+    <div className="w-screen h-screen">
+      <Button onClick={toggleTheme}>
+        <div className="capitalize">
+          {" "}
+          Switch To {theme === "dark" ? "Light" : "Dark"}{" "}
+        </div>
+      </Button>
+      <div className="flex items-center gap-4">
+        {themes.map((theme: string) => {
+          return <ThemeBox key={theme} theme={theme} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
